b7741363546d6bd6bd7585496c9ef1796d16b19e
Tommaso Martorella <tom.martorella@gmail.com>
2025-07-28 12:34:33 +0000

    experiments

---
+++ experiments/fid/uncond_openai.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#SBATCH --job-name=fid-evaluation
+#SBATCH --partition=gpu_lowp  # Specify the partition name
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4         # Adjust based on your needs
+#SBATCH --gres=gpu:h100:1               # Number of GPUs per node
+#SBATCH --mem=48G                  # Adjust based on your needs
+#SBATCH --time=24:00:00            # Adjust based on your needs
+#SBATCH --output=/nfs/ghome/live/martorellat/guided-diffusion/logs/%j/log.out
+#SBATCH --error=/nfs/ghome/live/martorellat/guided-diffusion/logs/%j/log.err
+
+# # Exit on errors
+set -o errexit
+
+EXPERIMENT_NAME="cifar10_uncond_openai"
+CHECKPOINT_STEP="300000"
+
+echo "Checkpoint: $EXPERIMENT_NAME"
+echo "Checkpoint step: $CHECKPOINT_STEP"
+
+SAMPLING_STEPS=(2 3 4 5 10 30 50 100 250 500 1000)
+CFG_SCALES=(0.0)
+SAMPLING_MODE="DDIM"
+NUM_FID_SAMPLES=50000
+
+export OPENAI_LOGDIR="/ceph/scratch/martorellat/guided_diffusion/logs_$EXPERIMENT_NAME"
+export OPENAI_BLOBDIR="/ceph/scratch/martorellat/guided_diffusion/blobs_$EXPERIMENT_NAME"
+
+for N_STEPS in "${SAMPLING_STEPS[@]}"
+do
+    for CFG_SCALE in "${CFG_SCALES[@]}"
+    do
+        echo "Running sampling with $N_STEPS steps and CFG scale $CFG_SCALE"
+        export OPENAI_SAMPLESDIR="/ceph/scratch/martorellat/guided_diffusion/samples_$EXPERIMENT_NAME/$SAMPLING_MODE-steps-$N_STEPS"
+        # Set number of processes per node based on local mode
+        USE_DDIM="False"
+        N_STEPS_FORMATTED=$N_STEPS
+        if [ "$SAMPLING_MODE" = "DDIM" ]; then
+            USE_DDIM="True"
+            N_STEPS_FORMATTED="ddim$N_STEPS"
+        fi
+        uv run python \
+            scripts/image_sample.py \
+            --data_dir "/nfs/ghome/live/martorellat/data/cifar_train" \
+            --model_path "/ceph/scratch/martorellat/guided_diffusion/blobs_$EXPERIMENT_NAME/ema_0.9999_$CHECKPOINT_STEP.pt" \
+            --image_size 32 \
+            --num_classes 10 \
+            --num_channels 128 \
+            --num_res_blocks 3 \
+            --learn_sigma True \
+            --batch_size 1024 \
+            --diffusion_steps 4000 \
+            --noise_schedule cosine \
+            --use_ddim $USE_DDIM \
+            --timestep_respacing $N_STEPS_FORMATTED \
+            --num_samples $NUM_FID_SAMPLES \
+            --exp_name $EXPERIMENT_NAME
+
+        # Important to cd because we have another venv inside evaulations
+        # and uv picks it automatically if we are in the right folder
+        cd evaluations
+        uv run python \
+            evaluator.py \
+            /nfs/ghome/live/martorellat/data/images_train.npz \
+            /ceph/scratch/martorellat/guided_diffusion/samples_$EXPERIMENT_NAME/$SAMPLING_MODE-steps-$N_STEPS/samples_${NUM_FID_SAMPLES}x32x32x3.npz
+        cd ..
+        
+
+    done
+done
+
+# Plot FID
+uv run python \
+    scripts_extra/plot_fid.py \
+    --samples_dir "/ceph/scratch/martorellat/guided_diffusion/samples_$EXPERIMENT_NAME" \
+    --plot_out "/ceph/scratch/martorellat/guided_diffusion/samples_$EXPERIMENT_NAME" \
+    --exp_name "$EXPERIMENT_NAME" \
+    --sampling_steps "${SAMPLING_STEPS[@]}" \
---
+++ experiments/train_distributional_noweighting_lambda_sigmoid.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#SBATCH --job-name=sit-training
+#SBATCH --partition=gpu_lowp  # Specify the partition name
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=8         # Adjust based on your needs
+#SBATCH --gres=gpu:h100:2               # Number of GPUs per node
+#SBATCH --mem=48G                  # Adjust based on your needs
+#SBATCH --time=24:00:00            # Adjust based on your needs
+#SBATCH --output=/nfs/ghome/live/martorellat/guided-diffusion/logs/%j/log.out
+#SBATCH --error=/nfs/ghome/live/martorellat/guided-diffusion/logs/%j/log.err
+
+# Exit on errors
+set -o errexit
+
+# Parse command line arguments
+LOCAL_MODE=false
+for arg in "$@"; do
+    case $arg in
+        --local)
+            LOCAL_MODE=true
+            shift
+            ;;
+        *)
+            # Unknown option, ignore
+            ;;
+    esac
+done
+
+# Set environment variables for wandb logging (optional)
+export WANDB_KEY="71b54366f0dcf364f47a59ed91fd5e5db58a0928"
+export ENTITY="tommaso_research"
+export PROJECT="sit_training"
+export EXPERIMENT_NAME="cifar10_cond_distributional_noweighting_lambda_sigmoid"
+
+export OPENAI_LOGDIR="/ceph/scratch/martorellat/guided_diffusion/logs_$EXPERIMENT_NAME"
+export OPENAI_BLOBDIR="/ceph/scratch/martorellat/guided_diffusion/blobs_$EXPERIMENT_NAME"
+
+# --- Training ---
+# See the README for more options: https://github.com/willisma/SiT#training-sit [1]
+
+DATA_PATH="/nfs/ghome/live/martorellat/data/cifar_train" # Specify the path to your ImageNet training data
+POPULATION_SIZE=4
+
+# Set number of processes per node based on local mode
+if [ "$LOCAL_MODE" = true ]; then
+    # Launch the training using torchrun
+    uv run python -m debugpy --listen 0.0.0.0:5678 --wait-for-client ./scripts/image_train.py \
+        --data_dir $DATA_PATH \
+        --image_size 32 \
+        --num_classes 10 \
+        --num_channels 128 \
+        --num_res_blocks 3 \
+        --class_cond True \
+        --learn_sigma True \
+        --lr 1e-4 \
+        --batch_size $((256 / $POPULATION_SIZE)) \
+        --dropout 0.3 \
+        --diffusion_steps 4000 \
+        --noise_schedule cosine \
+        --predict_xstart True \
+        --use_distributional True \
+        --distributional_loss_weighting NO_WEIGHTING \
+        --distributional_population_size $POPULATION_SIZE \
+        --distributional_lambda_weighting sigmoid
+
+else
+    echo "Running in SLURM mode with $SLURM_GPUS_ON_NODE GPUs"
+    mpiexec -n $SLURM_GPUS_ON_NODE uv run ./scripts/image_train.py \
+        --data_dir $DATA_PATH \
+        --image_size 32 \
+        --num_classes 10 \
+        --num_channels 128 \
+        --num_res_blocks 3 \
+        --class_cond True \
+        --learn_sigma True \
+        --lr 1e-4 \
+        --batch_size $(((256 / $POPULATION_SIZE) / $SLURM_GPUS_ON_NODE)) \
+        --dropout 0.3 \
+        --diffusion_steps 4000 \
+        --noise_schedule cosine \
+        --predict_xstart True \
+        --use_distributional True \
+        --distributional_loss_weighting NO_WEIGHTING \
+        --distributional_population_size $POPULATION_SIZE \
+        --distributional_lambda_weighting sigmoid
+
+fi
